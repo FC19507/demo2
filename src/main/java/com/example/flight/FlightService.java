9da88ac6cf6b4ce924c3b3d183ce324efdaa6400
FC19507 <nemichand.kumawat@freecharge.com>
2021-10-01 10:50:02 +0530

    ASSIGNEMT2

---
+++ src/main/java/com/example/flight/FlightService.java
@@ -0,0 +1,11 @@
+package com.example.flight;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightService {
+	
+ public void method1(Flight flight) {
+	 System.out.println("Service method called");
+ }
+}
